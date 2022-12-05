b5eb56ee188e9eb0d3c1903d38d02281c284d856
Evelyn O <evelynolives@gmail.com>
2022-12-05 14:14:58 -0300

    add game elements

---
+++ index.js
@@ -0,0 +1,46 @@
+// ELEMENTS
+let gameStart;
+let gameBall;
+let gameSystem;
+let gamePlayer;
+let gameScoreboard;
+
+
+//KEYBOARD CONTROL
+let keyControl;
+
+
+//ANIMATION CONTROL
+let frames;
+
+
+//POSITIONS
+let posBallX, posBballY;
+let posPlayerX, posPlayerY;
+let posSystemX, posSystemY;
+
+
+//INITIAL DIRECTIONS
+let playerInitialY = 180;
+let playerInitialX = 10;
+let systemInitialX = 180;
+let systemInitialY = 180;
+let ballInitialX = 475;
+let ballInitialY = 240;
+
+
+//AREA
+let areaX = 0, areaY = 960, areaW = 960, areaH = 500;
+let barW = 20, barH = 140;
+let ballAreaW = 20, ballAreaH = 20;
+let ballW = 20, ballH = 0;
+
+
+//DIRECTIONS
+let ballX, ballY;
+let systemY = 0;
+
+
+//SPEED
+let speedBall, speedSystem, seedPlayer;
+
